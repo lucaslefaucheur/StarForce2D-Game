e62ea3d2fd2b18391f3a1affc6fec08ecc44d764
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2018-09-25 17:59:11 -0400

    Game setup, Player and Camera

---
+++ Star Force/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public float xMin, xMax, yMin, yMax;
+
+    private Rigidbody rb;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+            (
+                Mathf.Clamp(rb.position.x, xMin, xMax),
+                Mathf.Clamp(rb.position.y, yMin, yMax),
+                0.0f
+            );
+        rb.rotation = Quaternion.Euler(-90.0f, rb.velocity.x * -tilt, 0.0f); // facultatif (ca rajoute du tilt
+    }
+}
