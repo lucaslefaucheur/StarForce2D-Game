e60dd0fc6559998dc149eb497751604115c287c8
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2018-09-26 16:57:16 -0400

    boundaries added

---
+++ Star Force/Assets/Scripts/DestroyByBoudary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoudary : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
