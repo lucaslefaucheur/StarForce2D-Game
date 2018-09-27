31d2431ad603da8eeabb0e20503ba26330e7ce4b
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2018-09-27 14:57:53 -0400

    Enemies movement

---
+++ Star Force/Assets/Scripts/EnemyAController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAController : MonoBehaviour {
+
+    int direction = 1;
+
+	void Update () 
+    {
+        if (gameObject.transform.position.y >= 18)
+            direction = 1;
+
+        if (gameObject.transform.position.y <= 4)
+            direction = -1;
+
+        transform.Translate(0, 0, 2 * direction * Time.deltaTime);
+    }
+}
--- Star Force/Assets/Scripts/GameController.cs
+++ Star Force/Assets/Scripts/GameController.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	
+    public GameObject EnemyShipA;
+    public GameObject EnemyShipB;
+
+    private void Start()
+    {
+        SpawnWaves();
+    }
+
+    void SpawnWaves () 
+    {
+        Instantiate(EnemyShipA, new Vector3(-8, 23, 0), EnemyShipA.transform.rotation);
+        Instantiate(EnemyShipA, new Vector3(-4, 21, 0), EnemyShipA.transform.rotation);
+        Instantiate(EnemyShipA, new Vector3(0, 19, 0), EnemyShipA.transform.rotation);
+        Instantiate(EnemyShipA, new Vector3(4, 21, 0), EnemyShipA.transform.rotation);
+        Instantiate(EnemyShipA, new Vector3(8, 23, 0), EnemyShipA.transform.rotation);
+    }
 }
